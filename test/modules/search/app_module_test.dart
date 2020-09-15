84d68548d3360b21b0040dc249dd1b9a0a7a9da8
Rodrigo Morais Rodarte <rodarte.rodrigo@hotmail.com>
2020-09-14 22:17:15 -0300

    final test unit

--- test/modules/search/app_module_test.dart
+++ test/modules/search/app_module_test.dart
@@ -2,7 +2,9 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutterarquitetureapp/modules/search/app_module.dart';
+import 'package:flutterarquitetureapp/modules/search/domain/entities/result_search.dart';
 import 'package:flutterarquitetureapp/modules/search/domain/usercases/search_by_text.dart';
+import 'package:mockito/mockito.dart';
 
 main(){
 
@@ -11,5 +13,11 @@ main(){
   test("Must return a usecase without error", (){
     final useCase = Modular.get<ISearchByText>();
     expect(useCase, isA<SearchByText>());
+  });
+
+  test("Must return a resultSearch list", ()async{
+    final useCase = Modular.get<ISearchByText>();
+    final result = await useCase("rodarte");
+    expect(result | null, isA<List<ResultSearch>>());
   });
 }
