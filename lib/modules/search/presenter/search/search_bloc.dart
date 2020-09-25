3dabe44f73fd7ee9873b97e63352b37ce2a71af4
Rodrigo Morais Rodarte <rodarte.rodrigo@hotmail.com>
2020-09-25 02:01:44 -0300

    bloc adjust implementing transition

--- lib/modules/search/presenter/search/search_bloc.dart
+++ lib/modules/search/presenter/search/search_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutterarquitetureapp/modules/search/domain/usercases/search_by_text.dart';
 import 'package:flutterarquitetureapp/modules/search/presenter/search/states/state.dart';
+import 'package:rxdart/rxdart.dart';
 
 class SearchBloc extends Bloc<String, SearchState>{
   final SearchByText usecase;
@@ -11,5 +12,12 @@ class SearchBloc extends Bloc<String, SearchState>{
     yield SearchLoad();
     final result = await usecase(searchText);
     yield result.fold((l) => SearchError(l), (r) => SearchSuccess(r));
+  }
+
+  @override
+  Stream<Transition<String, SearchState>> transformEvents(
+      Stream<String> events, transitionFn) {
+    events = events.debounceTime(Duration(milliseconds: 500));
+    return super.transformEvents(events, transitionFn);
   }
 }
